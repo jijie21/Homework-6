b3d7c59d80a084350b58c2862da3abfcc51ce06f
Jie(Jackie) Ji <35789241+jijie21@users.noreply.github.com>
2018-03-04 23:31:09 -0800

    Update and rename challenge2.js to challenge2-millisecond.js

---
+++ challenge2-millisecond.js
@@ -0,0 +1,34 @@
+//var millisecond = millis();
+var x = 0;
+var c;
+var colors;
+
+
+function setup() {
+  createCanvas(400, 400);
+  setTimeout(hello, 1000);
+  colorMode(HSB, 360, 1, 1);
+  colors = color(random(0,100), 50, 20);
+}
+
+function hello() {
+  noStroke(); 
+  c = color(random());
+  fill(c); 
+  ellipse(35, 35, 55, 55);
+  setTimeout(hello, 1000);
+
+}
+
+function draw() {
+  background(0);
+  stroke(random(0,100),50,20)
+  line(x, 0, x, height);
+  ellipse(35, 35, 55, 55);
+
+  if (x > width) {
+    x = 0
+  }
+  x = x + 3;
+  
+}
